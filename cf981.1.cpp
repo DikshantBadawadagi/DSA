120db60fe06d000e21debd78be73599f7acfc30e
DikshantBadawadagi <153322418+DikshantBadawadagi@users.noreply.github.com>
2024-10-25 00:46:39 +0530

    Add files via upload

---
+++ cf981.1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if(n %2 == 1){
+            cout<<"Kosuke"<<endl;
+        }
+        else{
+            cout<<"Sakurako"<<endl;
+        }
+
+    }
+}
