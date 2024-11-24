bf20258f77cfc5e4da766120fd68c603c33213b2
DikshantBadawadagi <153322418+DikshantBadawadagi@users.noreply.github.com>
2024-11-24 23:59:36 +0530

    Add files via upload

---
+++ cf988-4.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long t;
+    cin>>t;
+    while(t--)
+    {
+        long long n,m,l;
+        vector<pair<int,int>>h;
+        for(int i=0;i<n;i++){
+            cin>>h[i].first>>h[i].second;
+        }
+        vector<pair<int,int>>p;
+        for(int i=0;i<m;i++){
+            cin>>p[i].first>>p[i].second;
+        } 
+        
+    }
+    
+}
